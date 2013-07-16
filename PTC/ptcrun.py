2f007fe920ed8a3386203402c1740f1f74d2378d
Brent Townshend <bst@tc.com>
2013-07-16 13:25:30 -0700

    Fixed bug in VOLUME command

--- PTC/ptcrun.py
+++ PTC/ptcrun.py
@@ -6,7 +6,7 @@ if len(sys.argv)!=5 or ( sys.argv[2]!="BLOCK" and sys.argv[2]!="PROBE" and sys.a
     exit(2)
 
 p=ptc.PTC(5)
-res=p.execute("VOLUME %d",sys.argv[4]);
+res=p.execute("VOLUME %s"%sys.argv[4]);
 res=p.execute('RUN "%s",%s,%s'%(sys.argv[1],sys.argv[2],sys.argv[3]))
 status=p.getstatus()
 if (status.bsr & status.RUNNING) == 0:
