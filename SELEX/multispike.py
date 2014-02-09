3448c1a01f01bb46379d8ec6761873e450c1f682
Brent Townshend <bst@tc.com>
2014-02-09 15:59:02 -0800

    Added qPCR setup to multispike

--- SELEX/multispike.py
+++ SELEX/multispike.py
@@ -8,7 +8,7 @@ reagents=None
 srcprefix="A"
 firstround=1
 ndblrounds=4
-doqpcr=False
+doqpcr=True
 
 # Computation of PCR cycles
 rnagain=10    # Assumed DNA:RNA gain
@@ -38,10 +38,11 @@ print "Diversity at PCR2 input in second round = %.1g molecules"%(pcrinconc2*(pc
 for iteration in range(2):
     print "Iteration ",iteration+1
     trp=TRP()
-    input=["In"+srcprefix,"In"+srcprefix+"-spike"]
+    templates=["In"+srcprefix,"In"+srcprefix+"-spike"]
+    
     if iteration==0:
         #rnastore=Sample("RNA Storage",Experiment.REAGENTPLATE,None,None)
-        trp.addTemplates(input,inputConc*1e9)   # Add a template with stock concentration same as subsequent PCR products
+        trp.addTemplates(templates,inputConc*1e9)   # Add a template with stock concentration same as subsequent PCR products
     else:   
         reagents=Sample.getAllOnPlate(Experiment.REAGENTPLATE)
         for r in reagents:
@@ -54,15 +55,13 @@ for iteration in range(2):
         altprefix='B'
     else:
         altprefix='A'
-    sv=[]
-    svligtype=[]
-    svdil=[]
-    t7all=[]
+    ligsave=[]
+    pcrsave=[]
+    input=templates
     
     for round in range(ndblrounds):
         # Round 1 (Keep uncleaved +theo)
         t71=trp.runT7(theo=True,src=input,vol=10,srcdil=10.0/3,dur=15)
-        t7all=t7all+t71
 
         rt1=trp.runRT(pos=True,src=t71,vol=10,srcdil=2)
         trp.diluteInPlace(tgt=t71,dil=5)  # Dilute more to conserve
@@ -74,11 +73,12 @@ for iteration in range(2):
         prodbase="R%d-%c"%(firstround+round*2,currprefix)
         pcr1=trp.runPCR(prefix=currprefix,src=rt1,tgt=[prodbase,prodbase+"-spike"],vol=pcrvol,srcdil=4,ncycles=cycles1)
         trp.diluteInPlace(tgt=pcr1,dil=2)
-        sv1pcr=trp.saveSamps(src=pcr1,tgt=[prodbase+".SAVE",prodbase+"-spike.SAVE"],vol=25,dil=3,plate=trp.e.EPPENDORFS)
-    
+        eppie=trp.saveSamps(src=pcr1,tgt=[prodbase+".D3",prodbase+"-spike.D3"],vol=32,dil=3,plate=trp.e.EPPENDORFS)
+        # And save in dilution plate for qPCR (will need 400x dilution from this point) (also eppie may be removed before end)
+        pcrsave=pcrsave+trp.saveSamps(src=eppie,vol=5,dil=20,dilutant=trp.r.SSD, plate=trp.e.DILPLATE)
+
         # Round 2 (-theo, Ligate, keep cleaved)
         t72=trp.runT7(theo=False,src=pcr1,vol=10,srcdil=10.0/3)
-        t7all=t7all+t72
 
         rt2=trp.runRT(pos=True,src=t72,vol=10,srcdil=2)
         trp.diluteInPlace(tgt=t72,dil=5)  # Dilute more to conserve
@@ -92,17 +92,39 @@ for iteration in range(2):
             currprefix="B"
             
         # Run ligation of the first-half round too
-        lig2=trp.runLig(prefix=currprefix,src=sv1rt+rt2,vol=25,srcdil=3)
-        # Save ligation products at 5x dilution (for manual qPCR)
-        trp.saveSamps(src=lig2,vol=4,dil=10,dilutant=trp.r.SSD,plate=trp.e.DILPLATE)
+        lig2=trp.runLig(prefix=currprefix,src=sv1rt+rt2,vol=[15,15,25,25],srcdil=3)
+        # Save ligation products for qPCR (note that round 1 had 5x more dilution of RT product, so back that out here)
+        lig2[0:2]=trp.diluteInPlace(tgt=lig2[0:2],dil=5)
+        ligsave=ligsave+trp.saveSamps(src=lig2,vol=4,dil=16,dilutant=trp.r.SSD,plate=trp.e.DILPLATE)
 
         prodbase="R%d-%c"%(firstround+1+round*2,currprefix)
         pcr2=trp.runPCR(prefix=currprefix,tgt=[prodbase,prodbase+"-spike"],src=lig2[2:4],vol=pcrvol,srcdil=4,ncycles=cycles2)
         trp.diluteInPlace(tgt=pcr2,dil=2)
 
-        # Save PCR product in eppendorfs with 4x more dilution, ready for next round
-        trp.saveSamps(src=pcr2,tgt=[prodbase+".SAVE",prodbase+"-spike.SAVE"],vol=25,dil=3,plate=trp.e.EPPENDORFS)
+        # Save PCR product in eppendorfs for archive
+        eppie= trp.saveSamps(src=pcr2,tgt=[prodbase+".D3",prodbase+"-spike.D3"],vol=32,dil=3,plate=trp.e.EPPENDORFS)
+
+        # And save in dilution plate for qPCR (will need 400x dilution from this point)
+        pcrsave=pcrsave+trp.saveSamps(src=eppie,vol=5,dil=20, plate=trp.e.DILPLATE)
+        
+        # But use PCR product from sample plate for next rount to avoid extra dilution
         input=pcr2
+
+    if doqpcr:
+        # Setup qPCR
+        # Do A,B,M,T of input and PCR products (4*9), A,B of ligation products (2*8), M,T of BT423
+        # Saved products should already be at correct concentration for qPCR
+        #trp.e.w.setOptimization(True)
+        print "Setting up qPCR of %d ligation products with A,B primers"%(len(ligsave))
+        trp.runQPCR(src=ligsave,vol=15,srcdil=15.0/6,primers=["A","B"])
+        # Need to dilute templates to match PCR products
+        tmpldil1=trp.saveSamps(src=templates,vol=4,dil=20.0/3,plate=trp.e.DILPLATE)
+        tmpldil2=trp.saveSamps(src=tmpldil1+pcrsave,vol=5,dil=20,dilutant=trp.r.SSD,plate=trp.e.DILPLATE)
+        print "Setting up qPCR of %d template/PCR products with M,T primers"%(len(tmpldil2))
+        trp.runQPCR(src=tmpldil2,vol=15,srcdil=15.0/6,primers=["M","T"])
+        print "Setting up qPCR of %d template/PCR products with A,B primers"%(len(tmpldil2[0::2]))
+        trp.runQPCR(src=tmpldil2[0::2],vol=15,srcdil=15.0/6,primers=["A","B"])
+        trp.e.w.setOptimization(False)
 
 trp.finish()
 
