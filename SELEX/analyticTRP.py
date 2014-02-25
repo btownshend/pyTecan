c74d0ba17afe618cba05c4362aac0ed245578e7f
Brent Townshend <bst@tc.com>
2014-02-24 16:36:08 -0800

    Major overhaul

--- SELEX/analyticTRP.py
+++ SELEX/analyticTRP.py
@@ -5,64 +5,79 @@ import math
 from TRPLib.TRP import TRP
 import debughook
 
-# Configuration for this run (up to 15 total samples including replicates, 10 if also using QM)
-input=["BT423","R4A","R8A","528","530","531","BT532","BT533","BT534","BT535"]
-srcprefix=["A","A","A","A","A","A","A","A","A","A"];
-nreplicates=[1,1,1,1,1,1,1,1,1,1];
+# Configuration for this run (up to 15 total samples in reagent plate)
+input=["BT537","BT576","BT577","BT578","BT579","BT580","BT581","BT582","BT583","BT584","BT585","BT586","BT587","BT588","BT589"]
+srcprefix=["A","A","A","A","A","A","A","A","A","A","A","A","A","A","A"]
+nreplicates=[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+plustheo=[1,0,0,0,0,0,0,0,0,1,1,1,1,1,1]
+
+stockConc=10
+#primers=["A","M"]
+ligate=True
+primers=["A","B","M"]
 
 # Setup replicated inputs
-inputs=[];
-srcprefixes=[];
-prodprefixes=[];
+srcs=[]
+theo=[]
+srcprefixes=[]
+prodprefixes=[]
 for k in range(max(nreplicates)):
     for i in range(len(input)):
         if nreplicates[i]>k:
-            inputs=inputs+[input[i]];
-            srcprefixes=srcprefixes+[srcprefix[i]];
+            srcs=srcs+[input[i]]
+            theo=theo+[False]
+            srcprefixes=srcprefixes+[srcprefix[i]]
             if srcprefix[i]=='A':
-                prodprefixes=prodprefixes+['B'];
+                prodprefixes=prodprefixes+['B']
             else:
-                prodprefixes=prodprefixes+['A'];
-
+                prodprefixes=prodprefixes+['A']
+            if plustheo[i]:
+                srcs=srcs+[input[i]]
+                theo=theo+[True]
+                srcprefixes=srcprefixes+[srcprefix[i]]
+                if srcprefix[i]=='A':
+                    prodprefixes=prodprefixes+['B'];
+                else:
+                    prodprefixes=prodprefixes+['A'];
+                    
+                
 reagents=None
 
 for iteration in range(2):
     print "Iteration ",iteration+1
     trp=TRP()
     if iteration==0:
-        trp.addTemplates(input,2)   # Add a template with stock concentration 8nM
+        trp.addTemplates(input,stockConc,stockConc*24/80,plate=Experiment.EPPENDORFS)   # Add a template
     else:   
-        reagents=Sample.getAllOnPlate(Experiment.REAGENTPLATE)
+        reagents=Sample.getAllOnPlate(Experiment.REAGENTPLATE)+Sample.getAllOnPlate(Experiment.EPPENDORFS)
         for r in reagents:
             if r.volume<0:
                 r.initvolume=-r.volume+20
         Sample.clearall()
 
-
-#    t71=trp.runT7(theo=[False for i in inputs]+[True for i in inputs],src=inputs+inputs,tgt=[],vol=10,srcdil=80.0/24,dur=15)
-    t71=trp.runT7(theo=[y for x in inputs for y in [False,True]],src=[x for x in inputs for y in [False,True]],tgt=[],vol=10,srcdil=80.0/24,dur=15)
+    t71=trp.runT7(theo=theo,src=srcs,tgt=[],vol=10,srcdil=80.0/24,dur=15)
+    t71=trp.diluteInPlace(tgt=t71,dil=5)
+    # Dilute input samples enough to use in qPCR directly (should be 5000/(rnagain*2*5)  = 20)
+    qpcrdil1=trp.runQPCRDIL(src=t71,tgt=[],vol=100,srcdil=20,dilPlate=True)   
     rt1=trp.runRT(pos=True,src=t71,tgt=[],vol=5,srcdil=2)
-    trp.diluteInPlace(tgt=rt1,dil=4)
-    lig1=trp.runLig(prefix=prodprefixes+prodprefixes,src=rt1,tgt=[],vol=10,srcdil=3)
-
-    # Dilute input samples (can do this while ligation is still running)
-    qpcrdil1=trp.runQPCRDIL(src=inputs,tgt=[],vol=100,srcdil=40,dilPlate=True)   
-
-    # Dilute positive ligation products (this will wait for PTC to finish)
-    poslig=[s for s in lig1 if s[0:3]!="Neg"]
-    # Less further dilution for negative ligation products (use directly in qPCR)
-    neglig=[s for s in lig1 if s[0:3]=="Neg"]
-    trp.diluteInPlace(tgt=poslig,dil=10)
-    trp.diluteInPlace(tgt=neglig,dil=3)
-
-    # Compute remaining dilution needed
-    # Assume 25x RNA:DNA gain (needed to reduce dilution of DNA)
-    diltohere=[24/80.0 * 25 *40 for s in qpcrdil1]+[2*2*4*3*10 for s in poslig]  # Their dilution so far from the T7 product point (before stop added)
-    dilneeded=[5000.0/d for d in diltohere]
-
-    trp.e.w.userprompt("Load QPCR plate and press return to start QPCR setup")
-    qpcrdil2=trp.runQPCRDIL(src=qpcrdil1+poslig,tgt=[],vol=100,srcdil=dilneeded,dilPlate=True)
-    trp.runQPCR(src=qpcrdil2+neglig,vol=15,srcdil=10.0/4,useMid=True)
+    if ligate:
+        rt1=trp.diluteInPlace(tgt=rt1,dil=4)
+        lig1=trp.runLig(prefix=prodprefixes,src=rt1,tgt=[],vol=10,srcdil=3)
+        # Dilute positive ligation products (this will wait for PTC to finish)
+        poslig=[s for s in lig1 if s[0:3]!="Neg"]
+        # Less further dilution for negative ligation products (use directly in qPCR)
+        neglig=[s for s in lig1 if s[0:3]=="Neg"]
+        poslig=trp.diluteInPlace(tgt=poslig,dil=10)
+        neglig=trp.diluteInPlace(tgt=neglig,dil=3)
+        prods=poslig+neglig
+    else:
+        rt1=trp.diluteInPlace(tgt=rt1,dil=20)
+        prods=trp.saveSamps(src=rt1,vol=8,dil=(5000/(2*5*2*20)),plate=trp.e.DILPLATE,dilutant=trp.r.SSD)
+        
+    trp.runQPCR(src=qpcrdil1[:9],vol=15,srcdil=10.0/4,primers=["A","M"])
+    trp.runQPCR(src=qpcrdil1[9:],vol=15,srcdil=10.0/4,primers=["M"])
+    trp.runQPCR(src=prods[:9],vol=15,srcdil=10.0/4,primers=["A","B","M"])
+    trp.runQPCR(src=prods[9:],vol=15,srcdil=10.0/4,primers=["A","M"])
 
 trp.finish()
 
