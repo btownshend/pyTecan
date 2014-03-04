8f3bc7a6edfc2aa3cc68bf815ce20e1383ff9658
Brent Townshend <bst@tc.com>
2014-03-04 10:39:38 -0800

    Increased volume in initial dilution of ligation products

--- SELEX/multispike.py
+++ SELEX/multispike.py
@@ -57,7 +57,8 @@ for iteration in range(2):
         altprefix='B'
     else:
         altprefix='A'
-    ligsave=[]
+    ligsave1=[]
+    ligsave2=[]
     pcrsave=[]
     input=templates
     
@@ -96,9 +97,8 @@ for iteration in range(2):
         lig2=trp.runLig(prefix=currprefix,src=sv1rt+rt2,vol=[17,17,25,25],srcdil=3)
         lig2=trp.diluteInPlace(tgt=lig2,dil=1.33)
         # Save ligation products for qPCR (note that round 1 had more dilution of RT product, so back that out here)
-        ligsave1=trp.saveSamps(src=lig2[:2],vol=5,dil=6,plate=trp.e.DILPLATE,dilutant=trp.r.SSD)
-        ligsave2=trp.saveSamps(src=lig2[2:],vol=5,dil=6*3.5,plate=trp.e.DILPLATE,dilutant=trp.r.SSD)
-        ligsave=ligsave+ligsave1+ligsave2
+        ligsave1=ligsave1+trp.saveSamps(src=lig2[:2],vol=5,dil=20,plate=trp.e.DILPLATE,dilutant=trp.r.SSD)
+        ligsave2=ligsave2+trp.saveSamps(src=lig2[2:],vol=5,dil=20,plate=trp.e.DILPLATE,dilutant=trp.r.SSD)
 
         prodbase="R%d-%c"%(firstround+1+round*2,currprefix)
         pcr2=trp.runPCR(prefix=currprefix,tgt=[prodbase,prodbase+"-spike"],src=lig2[2:4],vol=pcrvol,srcdil=4,ncycles=cycles2)
@@ -118,15 +118,16 @@ for iteration in range(2):
         # Do A,B,M,T of input and PCR products (4*9), A,B of ligation products (2*8), M,T of reference
         # Saved products should already be at correct concentration for qPCR
         #trp.e.w.setOptimization(True)
-        print "Setting up qPCR of %d ligation products with A,B primers"%(len(ligsave))
-        ligtmp1=trp.saveSamps(src=ligsave,vol=4,dil=25,plate=trp.e.DILPLATE,dilutant=trp.r.SSD)
+        print "Setting up qPCR of %d ligation products with A,B primers"%(len(ligsave1+ligsave2))
+        ligtmp1=trp.saveSamps(src=ligsave1,vol=4,dil=8,plate=trp.e.DILPLATE,dilutant=trp.r.SSD)
+        ligtmp2=trp.saveSamps(src=ligsave2,vol=4,dil=8*3.5,plate=trp.e.DILPLATE,dilutant=trp.r.SSD)
         # Need to dilute templates to match PCR products
         tmpldil1=trp.saveSamps(src=templates,vol=4,dil=20.0/3,plate=trp.e.DILPLATE)
         tmpldil2=trp.saveSamps(src=tmpldil1+pcrsave,vol=4,dil=20,plate=trp.e.DILPLATE) 
         tmpldil2b=trp.saveSamps(src=["BT371"],vol=2,dil=50,plate=trp.e.DILPLATE) 
         tmpldil3=trp.saveSamps(src=tmpldil2+tmpldil2b,vol=4,dil=16,dilutant=trp.r.SSD,plate=trp.e.DILPLATE)
 
-        trp.runQPCR(src=ligtmp1,vol=15,srcdil=15.0/6,primers=["A","B"])
+        trp.runQPCR(src=ligtmp1+ligtmp2,vol=15,srcdil=15.0/6,primers=["A","B"])
         notSpiked=[t for t in tmpldil3 if "spike" not in  t]
         spiked=[t for t in tmpldil3 if "spike" in t]
         print "Setting up qPCR of  %d non-spiked template/PCR products with A,B,M,T primers"%(len(notSpiked))
