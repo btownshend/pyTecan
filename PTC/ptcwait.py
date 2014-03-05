24def097a99897e49f383dac67f2066103f37770
Brent Townshend <bst@tc.com>
2014-03-04 21:13:28 -0800

    Improved output in ptcwait

--- PTC/ptcwait.py
+++ PTC/ptcwait.py
@@ -3,26 +3,31 @@ import time
 import logging
 
 p=ptc.PTC()
+firstpass=True
+
 logging.info('Waiting for PTC to complete')
 #p.setdebug()
 while True:
     status=p.getstatus()
-    print "\n"
-    logging.info("Status=%s",status)
     p.execute("HTIME?")
     p.execute("RTIME?")
 
     stat=p.getstatus()
-    if stat.bsr&stat.RUNNING == 0:
-        logging.warning( "No program running")
-        break
-    etime=stat.etime-stat.stime
-    minutes=int(etime/60);
-    seconds=etime-minutes*60;
     pgmstatus="Running"
     if stat.bsr&stat.PAUSED:
         pgmstatus= "PAUSED"
-        logging.info( "%s %s step %d cycle %d/%d: '%s', T=%.1f, Lid=%.1f, step elapsed=%.0f seconds, total remaining=%d:%02d"%(pgmstatus,stat.pgm,stat.step,stat.cycle,stat.cycles,stat.cmd,stat.calctemp,stat.lidtemp,stat.stime,minutes,seconds))
+    if stat.bsr&stat.RUNNING == 0:
+        pgmstatus="NOT RUNNING"
+    etime=stat.etime-stat.stime
+    minutes=int(etime/60);
+    seconds=etime-minutes*60;
+
+    print  "%s %s step %d cycle %d/%d: '%s', T=%.1f, Lid=%.1f, step elapsed=%.0f seconds, total remaining=%d:%02d"%(pgmstatus,stat.pgm,stat.step,stat.cycle,stat.cycles,stat.cmd,stat.calctemp,stat.lidtemp,stat.stime,minutes,seconds)
+    if stat.bsr&stat.RUNNING == 0:
+        if firstpass:
+            logging.warning( "No program running")
+        break
+    firstpass=False
     if etime<=0.0:
         logging.info( "Proceeding to next step")
         p.execute("PROCEED")
