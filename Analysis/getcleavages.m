166ac67e944b93c27e1d151e7ea6849114bcd8d8
Brent Townshend <bst@tc.com>
2014-12-05 00:11:08 -0800

    Compute cleavages for new A/B ligations

--- Analysis/getcleavages.m
+++ Analysis/getcleavages.m
@@ -9,46 +9,69 @@ types=cellfun(@(z) z.type, data.results,'UniformOutput',false);
 utmpls=unique(tmpls);
 for i=1:length(utmpls)
   tmpl=utmpls{i};
-  %  fprintf('Processing %s\n', tmpl);
+  fprintf('Processing %s\n', tmpl);
   % Locate template measurement and ligation measurement
   indtmpl=find(strcmp(tmpls,tmpl)&strcmp(types,'tmpl'));
   indlig=find(strcmp(tmpls,tmpl) &strcmp(types,'Lig'));
-  if length(indtmpl)~=1 || length(indlig)~=1
-    fprintf('getcleavages: Unable to locate tmpl/lig product for %s\n', tmpl);
+  if length(indtmpl)~=1
+    fprintf('getcleavages: Unable to locate template for %s\n', tmpl);
+    continue;
+  end
+  if length(indlig)~=1 && length(indlig)~=2
+    fprintf('getcleavages: Unable to locate ligation product for %s\n', tmpl);
     continue;
   end
   
-  lig=data.results{indlig};
-  t=data.results{indtmpl};
+  for ii=1:length(indlig)
+    lig=data.results{indlig(ii)};
+    t=data.results{indtmpl};
   
-  % Figure out prefix of template, ligation product
-  pairs={{'WX','AX'},{'AS','BS'},{'BS','AS'},{'BS','WS'},{'AX','BX'},{'BX','AX'}};
-  thepair=[];
-  for j=1:length(pairs)
-    p=pairs{j};
-    if isfield(t,p{1}) && isfield(lig,p{1}) && isfield(lig,p{2}) && p{2}(1)==lig.ligprefix
-      thepair=p;
-      break;
+    % Figure out prefix of template, ligation product
+    pairs={{'WX','AX'},{'AS','BS'},{'BS','AS'},{'BS','WS'},{'AX','BX'},{'BX','AX'},{'MX','AX'},{'MX','BX'}};
+    thepair=[];
+    for j=1:length(pairs)
+      p=pairs{j};
+      if isfield(t,p{1}) && isfield(lig,p{1}) && isfield(lig,p{2}) && p{2}(1)==lig.ligprefix
+        thepair=p;
+        break;
+      end
     end
+    if isempty(thepair)
+      fprintf('Unable to determine template/ligation prefixes\n');
+      keyboard
+      continue;
+    end
+    lig.ucpair=thepair;
+    
+    if strcmp(thepair{1},'MX')
+      % New method: MX is total including template, AX is uncleaved only, BX is cleaved only
+      total=lig.(thepair{1}).conc;
+      tmplc=t.(thepair{1}).conc;
+      if strcmp(thepair{2},'AX')
+        unclvd=lig.(thepair{2}).conc;
+        yield=total-tmplc;
+        clvd=yield-unclvd;
+      else
+        clvd=lig.(thepair{2}).conc;
+        yield=total-tmplc;
+        unclvd=yield-clvd;
+      end
+    else
+      clvd=lig.(thepair{2}).conc;
+      unclvd=lig.(thepair{1}).conc-t.(thepair{1}).conc;
+      yield=clvd+unclvd;
+    end
+    
+    if unclvd<0
+      fprintf('Warning: %s has less template in the ligation product than it started with\n', tmpl);
+      continue;
+    end
+    lig.cleavage=clvd/(clvd+unclvd);
+    lig.yield=yield;
+    lig.rnagain=yield/t.(thepair{1}).conc;
+    
+    data.results{indlig(ii)}=lig;
   end
-  if isempty(thepair)
-    fprintf('Unable to determine template/ligation prefixes\n');
-    keyboard
-    continue;
-  end
-  lig.ucpair=thepair;
-  
-  clvd=lig.(thepair{2}).conc;
-  unclvd=lig.(thepair{1}).conc-t.(thepair{1}).conc;
-  if unclvd<0
-    fprintf('Warning: %s has less template in the ligation product than it started with\n', tmpl);
-    continue;
-  end
-  lig.cleavage=clvd/(clvd+unclvd);
-  lig.yield=clvd+unclvd;
-  lig.rnagain=lig.yield/t.(thepair{1}).conc;
-  
-  data.results{indlig}=lig;
 end
 
 
