f1534eaa197a58013eefb67501f3c9435d9dfb7d
Brent Townshend <bst@tc.com>
2015-01-21 18:31:39 -0800

    Added BOIL program for lid testing

--- PTC/ptcprogram.py
+++ PTC/ptcprogram.py
@@ -10,5 +10,6 @@ p.program('TRP','TRPLIG',['TEMP 16,1800','TEMP 65,600','TEMP 25,30'])
 p.program('TRP','LIG15RT',['TEMP 25,900','TEMP 65,600','TEMP 25,30'])
 p.program('TRP','COOLDOWN',['TEMP 16,60'])
 p.program('TRP','DRY',['TEMP 95,300','TEMP 25,1'])
+p.program('TRP','BOIL',['TEMP 105,600','TEMP 25,6000'])
 pgms=p.programs("TRP")
 print "pgms=",pgms
