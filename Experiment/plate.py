27a791eaa8389f00b1df6d2ce1fd9f5b0461a04b
Brent Townshend <bst@tc.com>
2015-03-02 20:01:40 -0800

    Added getliquidheight

--- Experiment/plate.py
+++ Experiment/plate.py
@@ -1,3 +1,5 @@
+import math
+
 "An object representing a microplate or other container on the deck"
 class Plate(object):
     "A plate object which includes a name, location, and size"
@@ -15,7 +17,30 @@ class Plate(object):
         self.grid=otherplate.grid
         self.pos=otherplate.pos
         self.unusableVolume=otherplate.unusableVolume
-        
+
+    def getliquidheight(self,volume):
+        'Get liquid height in mm above ZMax'
+        if self.name=="Samples":
+            if volume>50:
+                height=4.4+.049*volume
+            else:
+                height=6.9*volume/50
+        elif self.name=="Reagents":
+            h0=-10.01
+            h1=16.91
+            r1=4.00
+            v0=21.8
+            v1=math.pi/3*(h1-h0)*r1*r1-v0;
+            if volume>=v1:
+                height=(volume-v1)/(math.pi*r1*r1)+h1
+            else:
+                height=((volume+v0)*(3/math.pi)*((h1-h0)/r1)**2)**(1.0/3)+h0
+            print "vol=%.1f, height=%.1f"%(volume,height)
+        else:
+            print "No liquid height equation for plate %s"%self.name
+            assert(0)
+        return height
+    
     def wellname(self,well):
         if well==None:
             return "None"
