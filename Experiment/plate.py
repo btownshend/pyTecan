9236ae5c4672481b397218f98a5a498cd98a544d
Brent Townshend <bst@tc.com>
2015-03-03 00:32:11 -0800

    Updated getliquidheight equations

--- Experiment/plate.py
+++ Experiment/plate.py
@@ -12,6 +12,7 @@ class Plate(object):
         self.pierce=pierce
         self.unusableVolume=unusableVolume
         self.maxVolume=maxVolume
+        self.warned=False
         
     def movetoloc(self,otherplate):
         self.grid=otherplate.grid
@@ -20,25 +21,36 @@ class Plate(object):
 
     def getliquidheight(self,volume):
         'Get liquid height in mm above ZMax'
+        angle=17.5*math.pi/180;
+        # Use data from Robot/Calibration/20150302-LiquidHeights
         if self.name=="Samples":
-            if volume>50:
-                height=4.4+.049*volume
-            else:
-                height=6.9*volume/50
+            if volume<60 and not self.warned:
+                print "%s plate liquid heights not validated for <60 ul"%self.name
+                self.warned=True
+            r1=2.56;
+            h1=9.64;
+            v0=9.9;
         elif self.name=="Reagents":
-            h0=-10.01
-            h1=16.91
-            r1=4.00
-            v0=21.8
-            v1=math.pi/3*(h1-h0)*r1*r1-v0;
-            if volume>=v1:
-                height=(volume-v1)/(math.pi*r1*r1)+h1
-            else:
-                height=((volume+v0)*(3/math.pi)*((h1-h0)/r1)**2)**(1.0/3)+h0
-            print "vol=%.1f, height=%.1f"%(volume,height)
+            h1=16.69
+            r1=3.99
+            v0=14.7
+        elif self.name=="qPCR":
+            if volume<110 and not self.warned:
+                print "%s plate liquid heights not validated for <110 ul"%self.name
+                self.warned=True
+            h1=10.31
+            r1=2.65
+            v0=7.5
         else:
             print "No liquid height equation for plate %s"%self.name
             assert(0)
+        h0=h1-r1/math.tan(angle/2);
+        v1=math.pi/3*(h1-h0)*r1*r1-v0;
+        if volume>=v1:
+            height=(volume-v1)/(math.pi*r1*r1)+h1
+        else:
+            height=((volume+v0)*(3/math.pi)*((h1-h0)/r1)**2)**(1.0/3)+h0
+            #        print "%s,vol=%.1f, height=%.1f"%(self.name,volume,height)
         return height
     
     def wellname(self,well):
