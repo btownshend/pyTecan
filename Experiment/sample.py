7c0345de0b119513522c0ebbd1da354a33920af3
Brent Townshend <bst@tc.com>
2015-03-08 21:12:59 -0700

    Only mix if >= unusableVolume

--- Experiment/sample.py
+++ Experiment/sample.py
@@ -334,7 +334,7 @@ class Sample(object):
             return False
         mixvol=self.volume;  # -self.plate.unusableVolume;  # Can mix entire volume, if air is aspirated, it will just be dispensed first without making a bubble
         well=[self.well if self.well!=None else 2**(tipMask-1)-1 ]
-        if mixvol<2:
+        if mixvol<self.plate.unusableVolume:
             #print "Not enough volume in sample %s to mix"%self.name
             self.history+="(UNMIXED)"
             return False
