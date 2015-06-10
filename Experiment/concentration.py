289c56c6652f8684f0bbc92230d5001f8107efc7
Brent Townshend <bst@tc.com>
2015-06-10 00:08:21 -0700

    More user-friendly formatting of concentrations

--- Experiment/concentration.py
+++ Experiment/concentration.py
@@ -3,19 +3,31 @@ class Concentration(object):
         self.stock=stock
         self.final=final
         self.units=units
+
+    @staticmethod
+    def conctostr(conc,units):
+        if conc<1 and units=='nM':
+            conc=conc*1000
+            units='pM'
+        if conc<1 and units=='pM':
+            conc=conc*1000
+            units='fM'
+        if conc>1000 and units=='pM':
+            conc=conc/1000
+            units='nM'
+        return "%.3g%s"%(conc,units)
+
     def __str__(self):
         if self.stock==None:
             return "None"
         elif self.final==None or (self.final==1.0 and self.units=='x'):
             if self.stock==0  or self.stock >=0.1:
-                return "%.2f%s"%(self.stock,self.units)
+                return self.conctostr(self.stock,self.units)
             else:
-                return "%.2g%s"%(self.stock,self.units)
+                return self.conctostr(self.stock,self.units)
         else:
-            if (self.stock==0  or self.stock >=0.1) and (self.final==0 or self.final>=0.1):
-                return "%.2f%s->%.2f%s"%(self.stock,self.units,self.final,self.units)
-            else:
-                return "%.2g%s->%.2g%s"%(self.stock,self.units,self.final,self.units)
+            return self.conctostr(self.stock,self.units)+"->"+self.conctostr(self.final,self.units)
+
     def dilute(self,factor):
         if self.stock==None:
             return Concentration(None,None,'x')
@@ -24,3 +36,4 @@ class Concentration(object):
     def dilutionneeded(self):
         'Return dilution factor needed to dilute from stock to final'
         return self.stock*1.0/self.final
+        
