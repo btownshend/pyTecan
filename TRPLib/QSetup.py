d72d0a76a7d1ad7ded18d5121197b79e74f8b81f
Brent Townshend <bst@tc.com>
2016-04-11 14:16:57 -0700

    Remove unused findReuses() function

--- TRPLib/QSetup.py
+++ TRPLib/QSetup.py
@@ -99,49 +99,6 @@ class QSetup(object):
             self.dilProds=self.dilProds+[intermed]
             self.primers=self.primers+[primers]
             self.nreplicates=self.nreplicates+[nreplicates]
-    
-    def findReuses(self):
-        'Find any prior dilutions that can be reused'
-        maxstages=max(self.stages)
-        for j in range(len(self.samples)):
-            for i in range(len(self.samples)):
-                # Check if we can reuse i to form j
-                if i!=j and self.samples[i]==self.samples[j] and self.needDil[i]<self.needDil[j] and self.needDil[i]>1:
-                    # Possible reuse
-                    # Check if we already have a better one
-                    if self.reuse[j]!=None and self.needDil[self.reuse[j]]>self.needDil[i]:
-                        continue
-                    # Check if it would increase number of stages
-                    stages=int(math.ceil(math.log(self.needDil[j]/self.needDil[i])/math.log(self.MAXDIL))+self.stages[i])
-                    if stages>maxstages:
-                        continue
-                    print "Reuse %s@%f to form %s@%f"%(self.samples[i],self.needDil[i],self.samples[j],self.needDil[j])
-                    self.reuse[j]=i
-                    self.stages[j]=stages
-            totalDil=self.needDil[j]
-            stages=self.stages[j]
-            if self.reuse[j] != None:
-                totalDil = totalDil / self.needDil[self.reuse[j]]
-                stages = stages - self.stages[self.reuse[j]]
-            if stages>1:
-                print "Need to form %s@%f by diluting %f in %d stages "%(self.samples[j],self.needDil[j],totalDil,stages)
-                d=1
-                if self.reuse[j]!=None:
-                    d=self.needDil[self.reuse[j]]
-                for k in range(stages-1):
-                    d=min(d*self.MAXDIL,totalDil)
-                    self.addSamples([self.samples[j]],d,[])	# Add extra intermediate that can be reused
-                    if k==0:
-                        self.reuse[-1]=self.reuse[j]
-                    else:
-                        self.reuse[-1]=len(self.samples)-2
-                self.reuse[j]=len(self.samples)-1
-                
-        for i in range(len(self.samples)):
-            print "%d: %s@%f"%(i,self.samples[i],self.needDil[i]),
-            if self.reuse[i]!=None:
-                print ", use %d with additional %f dilution"%(self.reuse[i], self.needDil[i]/self.needDil[self.reuse[i]]),
-            print " [%d stages]"%self.stages[i]
                 
     def allprimers(self):
         return set([p for sublist in self.primers for p in sublist])
