7af897195e18511c28556e42739b3502d638e140
Brent Townshend <bst@tc.com>
2016-07-07 16:37:53 -0700

    Correct bug in USER incubation time during PCR (minutes not secs)

--- TRPLib/TRP.py
+++ TRPLib/TRP.py
@@ -640,7 +640,7 @@ class TRP(object):
         if usertime is None:
             worklist.pyrun('PTC\\ptcsetpgm.py %s TEMP@95,120 TEMP@95,10 TEMP@57,10 GOTO@2,%d TEMP@72,120 TEMP@25,2'%(pgm,ncycles-1))
         else:
-            worklist.pyrun('PTC\\ptcsetpgm.py %s TEMP@37,%d TEMP@95,120 TEMP@95,10 TEMP@57,10 GOTO@3,%d TEMP@72,120 TEMP@25,2'%(pgm,usertime,ncycles-1))
+            worklist.pyrun('PTC\\ptcsetpgm.py %s TEMP@37,%d TEMP@95,120 TEMP@95,10 TEMP@57,10 GOTO@3,%d TEMP@72,120 TEMP@25,2'%(pgm,usertime*60,ncycles-1))
         self.e.runpgm(pgm,4.80+1.55*ncycles,False,max(vol),hotlidmode="CONSTANT",hotlidtemp=100)
         self.e.shakeSamples(tgt,returnPlate=True)
         return tgt
