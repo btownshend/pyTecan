d8182fd9d586953f2d7c4ba346e182eb993d1bcd
Brent Townshend <bst@tc.com>
2016-12-06 00:31:17 -0800

    Added master3 runRxInPlace

--- TRPLib/TRP.py
+++ TRPLib/TRP.py
@@ -220,12 +220,13 @@ class TRP(object):
     ########################
     # Run a reaction in place
     ########################
-    def runRxInPlace(self,src,vol,master,master2=None,returnPlate=True,finalx=1.0):
+    def runRxInPlace(self,src,vol,master,master2=None,master3=None,returnPlate=True,finalx=1.0):
         'Run reaction on beads in given total volume'
-        [vol,src,master,master2]=listify([vol,src,master,master2])
+        [vol,src,master,master2,master3]=listify([vol,src,master,master2,master3])
         mastervol=[vol[i]*finalx/master[i].conc.dilutionneeded() for i in range(len(vol))]
         master2vol=[0 if master2[i] is None else vol[i]*finalx/master2[i].conc.dilutionneeded() for i in range(len(vol))]
-        watervol=[vol[i]-src[i].volume-mastervol[i]-master2vol[i] for i in range(len(vol))]
+        master3vol=[0 if master3[i] is None else vol[i]*finalx/master3[i].conc.dilutionneeded() for i in range(len(vol))]
+        watervol=[vol[i]-src[i].volume-mastervol[i]-master2vol[i]-master3vol[i] for i in range(len(vol))]
         if any([w < -0.02 for w in watervol]):
             logging.error("runRxInPlace: negative amount of water needed: %.2f"%min(watervol))
 
@@ -237,6 +238,8 @@ class TRP(object):
         for i in range(len(src)):
             if master2vol[i]>0:
                 self.e.transfer(master2vol[i],master2[i],src[i],(True,False))
+            if master3vol[i]>0:
+                self.e.transfer(master3vol[i],master3[i],src[i],(True,False))
         self.e.shakeSamples(src,returnPlate=returnPlate)
 
     ########################
