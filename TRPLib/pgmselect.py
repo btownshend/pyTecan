091dc59c133fa3f85648a040403f159502358bd8
Brent Townshend <bst@tc.com>
2017-07-10 10:58:44 -0700

    Updated concentration calculations

--- TRPLib/pgmselect.py
+++ TRPLib/pgmselect.py
@@ -84,11 +84,16 @@ class PGMSelect(TRP):
         stopConc=self.rnaConc*0.9
         rtConc=stopConc/self.rtDil
         rtdilConc=[rtConc/self.rtpostdil[i] for i in range(len(self.rounds))]
-        ligConc=[rtdilConc[i]/1.25 for i in range(len(self.rounds))]
-        ligdilConc=[ligConc[i]/self.extpostdil[i] for i in range(len(self.rounds))]
-        pcrConc=[ligConc[i]/self.pcrdil[i] for i in range(len(self.rounds))]
+        ligConc=[None if self.rounds[i]=='U' else rtdilConc[i]/1.25 for i in range(len(self.rounds))]
+        ligdilConc=[None if self.rounds[i]=='U' else ligConc[i]/self.extpostdil[i] for i in range(len(self.rounds))]
+        pcrConc=[rtConc/self.pcrdil[i] if self.rounds[i]=='U' else ligConc[i]/self.pcrdil[i] for i in range(len(self.rounds))]
         
-        print "Concs(nM):  RNA: %.0f, Stop: %.0f, RT: %.0f, RTDil: %.0f, Lig: %.0f, LigDil: %.0f, PCRIn: %.0f"%(self.rnaConc, stopConc, rtConc, rtdilConc[0], ligConc[0], ligdilConc[0], pcrConc[0])
+        for i in range(len(self.rounds)):
+            if ligConc[i] is None:
+                print "R%d Concs(nM):  RNA: %.0f, Stop: %.0f, RT: %.0f, RTDil: %.0f, PCRIn: %.0f"%(i, self.rnaConc, stopConc, rtConc, rtdilConc[i], pcrConc[i])
+            else:
+                print "R%d Concs(nM):  RNA: %.0f, Stop: %.0f, RT: %.0f, RTDil: %.0f, Lig: %.0f, LigDil: %.0f, PCRIn: %.0f"%(i, self.rnaConc, stopConc, rtConc, rtdilConc[i], ligConc[i], ligdilConc[i], pcrConc[i])
+
         self.pcrvol=[self.pcrcopies*self.pmolesIn*1000/pcrConc[i]/math.pow(self.enrich,(i+1.0)) for i in range(len(self.rounds))]
         # Use at least 100ul so the evaporation of the saved sample that occurs during the run will be relatively small
         self.pcrvol=[max(100,v) for v in self.pcrvol]
