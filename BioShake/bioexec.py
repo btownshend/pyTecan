0c19788c1efb969159811b824225e90c827fcfde
Brent Townshend <bst@tc.com>
2017-12-19 00:18:49 -0500

    Added retries to bioshake

--- BioShake/bioexec.py
+++ BioShake/bioexec.py
@@ -1,11 +1,19 @@
 import bioshake
 import sys
 import logging
+import time
 
 p=bioshake.BioShake()
 #logging.info( "Info=%s",p.info())
-res=p.execute(" ".join(sys.argv[1:]))
-logging.info( "Result=%s",res)
-if res!="ok":
-    sys.exit(1)
+cmd=" ".join(sys.argv[1:])
+for nattempts in range(3):
+    res=p.execute(cmd)
+    logging.info( "Result=%s",res)
+    if res=="ok":
+        sys.exit(0)
+    time.sleep(1)
+    logging.warning("Failed attempt %d",nattempts+1)
+
+logging.error("Failed %s",cmd)
+sys.exit(1)
     
