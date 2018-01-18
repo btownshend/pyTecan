89174bfe1f91d580eedf6781b98611865920b1b7
Brent Townshend <bst@tc.com>
2018-01-18 10:10:17 -0800

    Adjust formatting of pgmbarcode

--- TRPLib/pgmbarcode.py
+++ TRPLib/pgmbarcode.py
@@ -9,9 +9,9 @@ from TRPLib.QSetup import QSetup
 from TRPLib.TRP import TRP
 from TRPLib.mixsplit import mixsplit
 
-reagents.add("MPCR1", well="A1", conc=30.0/18.0,
-             ingredients={'Kapa': 3.33/2, 'USER': 1.67/2, 'glycerol': 2.5, 'Water': 95})
-reagents.add("MPCR2", well="B1", conc=10.0/9.0, ingredients={'Kapa': 2.22/2, 'glycerol': 2.22/2, 'Water': 97.78})
+reagents.add("MPCR1", well="A1", conc=30.0 / 18.0,
+             ingredients={'Kapa': 3.33 / 2, 'USER': 1.67 / 2, 'glycerol': 2.5, 'Water': 95})
+reagents.add("MPCR2", well="B1", conc=10.0 / 9.0, ingredients={'Kapa': 2.22 / 2, 'glycerol': 2.22 / 2, 'Water': 97.78})
 reagents.add("P-End", well="C1", conc=4)
 reagents.add("BT5310", well="D1", conc=Concentration(20, 20, "pM"))
 
@@ -19,8 +19,10 @@ reagents.add("BT5310", well="D1", conc=Concentration(20, 20, "pM"))
 class Barcoding(TRP):
     """Barcode multiple samples, mix them"""
 
-    def __init__(self, inputs,pcr1inputconc=0.05,used=[]):
+    def __init__(self, inputs, pcr1inputconc=0.05, used=None):
         super(Barcoding, self).__init__()
+        if used is None:
+            used = []
         self.inputs = inputs
 
         self.qconc = 50e-12  # Target qPCR concentration
@@ -43,11 +45,11 @@ class Barcoding(TRP):
                              initVol=self.bc1_inputvol, extraVol=0)
             else:
                 r = reagents.getsample(x['name'])
-                currConc = r.conc.stock
                 if r.conc.stock != x['conc']:
-                    logging.error('Input %s has conflicting concentrations set:  %f and %f',x['name'],r.conc.stock,x['conc'])
+                    logging.error('Input %s has conflicting concentrations set:  %f and %f', x['name'], r.conc.stock,
+                                  x['conc'])
                     assert False
-                    
+
         self.q = None  # Defined in pgm()
 
     def pgm(self):
@@ -61,10 +63,11 @@ class Barcoding(TRP):
                                right=[x['right'] for x in self.inputs])
 
         for i in range(len(self.inputs)):
-            x=self.inputs[i]
+            x = self.inputs[i]
             if 'bconc' in x and x['bconc'] is not None:
-                print "Resetting concentration of %s from expected %.1f to bconc setting of %.1f nM"%(x['name'],bcout[i].conc.stock,x['bconc'])
-                bcout[i].conc.stock=x['bconc']
+                print "Resetting concentration of %s from expected %.1f to bconc setting of %.1f nM" % \
+                      (x['name'], bcout[i].conc.stock, x['bconc'])
+                bcout[i].conc.stock = x['bconc']
 
         print "### qPCR #### (%.0f min)" % (clock.elapsed() / 60.0)
         self.q.run(confirm=False, enzName='EvaGreen')
@@ -72,11 +75,11 @@ class Barcoding(TRP):
         print "### Final PCR Done #### (%.0f min)" % (clock.elapsed() / 60.0)
         worklist.flushQueue()
 
-        if all(['bconc' in x and x['bconc'] is not None  for x in self.inputs]):
+        if all(['bconc' in x and x['bconc'] is not None for x in self.inputs]):
             print "### Mixdown #### (%.0f min)" % (clock.elapsed() / 60.0)
             worklist.comment('Start mixdown only at this point')
-            mixdown=self.mix(bcout, [x['weight'] for x in self.inputs])
-            #self.q.addSamples(mixdown, needDil=mixdown.conc.stock * 1e-9 / self.qconc, primers=self.qprimers,nreplicates=3)
+            mixdown = self.mix(bcout, [x['weight'] for x in self.inputs])
+            # self.q.addSamples(mixdown, needDil=mixdown.conc.stock * 1e-9 / self.qconc, primers=self.qprimers,nreplicates=3)
         else:
             print "### Not doing mixdown as bconc not set in all inputs"
 
@@ -104,50 +107,53 @@ class Barcoding(TRP):
 
     def oldmix(self, inp, weights, tgtdil=1.0):
         """Mix given inputs according to weights (by moles -- use conc.stock of each input)"""
-        print "Mix: tgtdil=%.2f, inp="%tgtdil,",".join(["%s@%.2f"%(inp[i].name,weights[i]) for i in range(len(inp))])
+        print "Mix: tgtdil=%.2f, inp=" % tgtdil, ",".join(
+            ["%s@%.2f" % (inp[i].name, weights[i]) for i in range(len(inp))])
         mixvol = 100.0
-        if len(inp)==1:
-            if tgtdil>1.0:
-                vol=[mixvol/tgtdil]
-                if vol[0]<4.0:
-                    vol[0]=4.0
+        if len(inp) == 1:
+            if tgtdil > 1.0:
+                vol = [mixvol / tgtdil]
+                if vol[0] < 4.0:
+                    vol[0] = 4.0
             else:
                 # Special case, just dilute 10x
-                vol=[mixvol/10]
+                vol = [mixvol / 10]
         else:
-            relvol = [weights[i] *1.0/ inp[i].conc.stock for i in range(len(inp))]
+            relvol = [weights[i] * 1.0 / inp[i].conc.stock for i in range(len(inp))]
             scale = mixvol / sum(relvol)
             for i in range(len(inp)):
-               if relvol[i] * scale > inp[i].volume - 16.4:
-                 scale = (inp[i].volume - 16.4) / relvol[i]
+                if relvol[i] * scale > inp[i].volume - 16.4:
+                    scale = (inp[i].volume - 16.4) / relvol[i]
             vol = [x * scale for x in relvol]
-            if min(vol)>4.0 and tgtdil>1.0:
-                scale=max(1.0/tgtdil,4.0/min(vol))
-                print "Rescale volumes by %.2f to get closer to target dilution of %.2f"%(scale, tgtdil)
+            if min(vol) > 4.0 and tgtdil > 1.0:
+                scale = max(1.0 / tgtdil, 4.0 / min(vol))
+                print "Rescale volumes by %.2f to get closer to target dilution of %.2f" % (scale, tgtdil)
                 vol = [x * scale for x in vol]
 
-        print "Mix1: vols=[",",".join(["%.2f"%v for v in vol]),"]"
+        print "Mix1: vols=[", ",".join(["%.3f" % v for v in vol]), "]"
         if min(vol) < 4.0:
             logging.info("Minimum volume into mixing would be only %.2f ul - staging..." % min(vol))
-            if max(vol)<4.0:
+            if max(vol) < 4.0:
                 # All volumes are low, just too much
                 # Split into 2 stages
-                sel=range(int(len(inp)/2))
-                nsel=range(int(len(inp)/2),len(inp))
+                sel = range(int(len(inp) / 2))
+                nsel = range(int(len(inp) / 2), len(inp))
             else:
                 # Choose a splitting threshold
-                mindilution=4.0/min(vol)
+                mindilution = 4.0 / min(vol)
                 thresh = np.median(vol)
-                while mixvol/sum([v for v in vol if v<thresh]) < mindilution:
-                    thresh = thresh*0.8
-                print "Using %.2f as threshold to split mixdown"%thresh
-                sel=[i for i in range(len(inp)) if vol[i]<thresh ]
-                nsel=[i for i in range(len(inp)) if vol[i]>=thresh ]
-            print "Mixing ",",".join([inp[i].name for i in sel])," with vols [",",".join(["%.2f"%vol[i] for i in sel]),"] in separate stage."
-            tgtdil=np.median([vol[i] for i in nsel])/sum([vol[i] for i in sel])
-            print "tgtdil=%.2f"%tgtdil
-            mix1=self.mix([inp[i] for i in sel],[weights[i] for i in sel],tgtdil)
-            mix2=self.mix([inp[i] for i in nsel]+[mix1],[weights[i] for i in nsel]+[sum([weights[i] for i in sel])])
+                while mixvol / sum([v for v in vol if v < thresh]) < mindilution:
+                    thresh = thresh * 0.8
+                print "Using %.2f as threshold to split mixdown" % thresh
+                sel = [i for i in range(len(inp)) if vol[i] < thresh]
+                nsel = [i for i in range(len(inp)) if vol[i] >= thresh]
+            print "Mixing ", ",".join([inp[i].name for i in sel]), " with vols [", ",".join(
+                ["%.2f" % vol[i] for i in sel]), "] in separate stage."
+            tgtdil = float(np.median([vol[i] for i in nsel]) / sum([vol[i] for i in sel]))
+            print "tgtdil=%.2f" % tgtdil
+            mix1 = self.mix([inp[i] for i in sel], [weights[i] for i in sel], tgtdil)
+            mix2 = self.mix([inp[i] for i in nsel] + [mix1],
+                            [weights[i] for i in nsel] + [sum([weights[i] for i in sel])])
             return mix2
         watervol = mixvol - sum(vol)
         print "Mixdown: vols=[", ",".join(["%.2f " % v for v in vol]), "], water=", watervol, ", total=", mixvol, " ul"
@@ -201,15 +207,17 @@ class Barcoding(TRP):
                 dilvol = s.volume * dil
                 if dilvol > 150.0:
                     maxdil = 150.0 / s.volume
-                    logging.info("Dilution of input %s (%.1f ul) by %.2f would require %.1f ul -- only diluting by %.1fx" % (
-                        s.name, s.volume, dil, dilvol, maxdil ))
-                    dil=maxdil
+                    logging.info(
+                        "Dilution of input %s (%.1f ul) by %.2f would require %.1f ul -- only diluting by %.1fx" % (
+                            s.name, s.volume, dil, dilvol, maxdil))
+                    dil = maxdil
                 self.diluteInPlace(tgt=[s], dil=dil)
                 print "Diluting %s by %.1f" % (s.name, dil)
 
         print "### PCR1 #### (%.0f min)" % (clock.elapsed() / 60.0)
 
-        pcr1 = self.runPCR(src=samps, srcdil=[s.conc.stock/self.pcr1inputconc for s in samps], ncycles=pcrcycles[0], vol=pcr1vol,
+        pcr1 = self.runPCR(src=samps, srcdil=[s.conc.stock / self.pcr1inputconc for s in samps], ncycles=pcrcycles[0],
+                           vol=pcr1vol,
                            primers=[[left[i], right[i]] for i in range(len(left))], usertime=30, fastCycling=False,
                            inPlace=False, master="MPCR1", kapa=True)
 
@@ -224,8 +232,6 @@ class Barcoding(TRP):
         for x in pcr1:
             x.conc = Concentration(stock=pcr1finalconc, units='nM')
 
-
-
         if len(pcrcycles) > 1:
             # Second PCR with 235p/236p on mixture (use at least 4ul of prior)
             print "### PCR2 #### (%.0f min)" % (clock.elapsed() / 60.0)
@@ -235,9 +241,9 @@ class Barcoding(TRP):
 
             pcr2finalconc = pcr1finalconc / (pcr2dil / pcr1postdil) * 2 ** pcrcycles[1]
             print "PCR2 final conc = %.1f nM" % pcr2finalconc
-            if pcr2finalconc>200:
+            if pcr2finalconc > 200:
                 print "Capping at 200nM"
-                pcr2finalconc=200
+                pcr2finalconc = 200
 
             for x in pcr2:
                 x.conc = Concentration(stock=pcr2finalconc, units='nM')
