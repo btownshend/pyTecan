1c7bb01adc6ed5b9fb134bb43973657ad3ffead9
Brent Townshend <bst@tc.com>
2018-03-01 13:44:18 -0800

    Use timestamps instead of datetimes

--- DB/mkrobot.sql
+++ DB/mkrobot.sql
@@ -11,11 +11,11 @@ use robot;
 create table runs(
        run varchar(36), primary key(run), 
        program varchar(50) not null, 
-       starttime datetime not null,
-       gentime datetime not null,
+       starttime timestamp not null default current_timestamp,
+       gentime timestamp not null default current_timestamp,
        checksum varchar(8) not null,
        gitlabel varchar(8) not null,
-       endtime datetime
+       endtime timestamp null
 );
 
 
@@ -26,7 +26,7 @@ create table ticks(
        run varchar(36) not null, 
        elapsed float not null, 
        remaining float, 
-       time datetime not null,
+       time timestamp not null default current_timestamp,
        foreign key(run) references runs(run) on delete cascade
 );
 
@@ -37,9 +37,9 @@ create table flags(
        run varchar(36)  not null, 
        name varchar(50)  not null, 
        value integer, 
-       lastupdate datetime not null,
+       lastupdate timestamp not null,
        
-       pulltime datetime,  -- Time this record was last pulled to robot
+       pulltime timestamp null,  -- Time this record was last pulled to robot
        foreign key(run) references runs(run) on delete cascade
 );
 
@@ -56,14 +56,14 @@ create table sampnames(
 -- Vols for a run
 -- Inserted on robot only, read-only on master, never updated
 create table vols(
-       vol integer, primary key(vol) auto_increment, 
+       vol integer primary key auto_increment, 
        run varchar(36), 
        plate varchar(20) not null, 
        well varchar(4) not null,
        gemvolume float not null,	  -- Volume as reported by Gemini
        volume float,   -- gemvolume converted to true volume
        expected float,   -- expected true volume
-       measured datetime not null,	-- when was measurement made
+       measured timestamp not null default current_timestamp,	-- when was measurement made
        foreign key(run) references runs(run) on delete cascade
 );
 
