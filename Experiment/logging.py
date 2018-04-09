fc4ade579198de0b57353bf7473d9afd72669a0b
Brent Townshend <bst@tc.com>
2018-04-09 16:18:59 -0700

    Added type hints to logging

--- Experiment/logging.py
+++ Experiment/logging.py
@@ -18,20 +18,20 @@ def callhistory(includeLibs=False):
             h=["%s:%d"%(nm[-1],f[2])]
     return h
 
-def notice(msg):
+def notice(msg: str) -> object:
     if globals.verbose:
         print("NOTICE: %s [%s]" % (msg, "->".join(callhistory())))
     
-def warning(msg,stderr=False):
+def warning(msg: str,stderr=False):
     if stderr:
         print("WARNING: %s [%s]"%(msg,"->".join(callhistory(globals.verbose))),file=sys.stderr)
     else:
         print("WARNING: %s [%s]"%(msg,"->".join(callhistory(globals.verbose))))
 
-def mixwarning(msg):
+def mixwarning(msg: str):
     print("MIXING: %s [%s]"%(msg,"->".join(callhistory(globals.verbose))))
 
-def error(msg,fatal=True):
+def error(msg: str,fatal=True):
     print("ERROR: %s [%s]"%(msg,"->".join(callhistory(True))))
     if fatal:
         assert False
