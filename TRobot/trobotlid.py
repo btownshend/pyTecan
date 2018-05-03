0d9a91230190f8cc0df5844809efc038bcf26b27
Brent Townshend <bst@tc.com>
2018-05-02 22:08:39 -0700

    Set trobot lid to retry multiple times

--- TRobot/trobotlid.py
+++ TRobot/trobotlid.py
@@ -8,30 +8,53 @@ if len(sys.argv)!=2 or ( sys.argv[1]!="OPEN" and sys.argv[1]!="CLOSE"):
     exit(2)
 cmd=sys.argv[1]
 p=trobot.TRobot()
+numAttempts=20
+logging.info("Status: %s"%str(p.getstatus()))
 
-lidstatus=p.getlidstatus()
-if cmd=="OPEN":
-    if lidstatus.isopen():
-        print "Already open"
-        exit(0)
-    p.lidopen()
-elif cmd=="CLOSE":
-    if lidstatus.isclosed():
-        print "Already closed"
-        exit(0)
-    p.lidclose()
+for attempt in range(numAttempts):
+    try:
+        lidstatus=p.getlidstatus()
+        if cmd=="OPEN":
+            if lidstatus.isopen():
+                print "Already open"
+                exit(0)
+            p.lidopen()
+        elif cmd=="CLOSE":
+            if lidstatus.isclosed():
+                print "Already closed"
+                exit(0)
+            p.lidclose()
 
-for i in range(100):
-    lidstatus=p.getlidstatus()
-    logging.info( "Lid status: %s"%str(lidstatus))
-    if cmd=="OPEN":
-        if lidstatus.isopen():
-            exit(0)
-    else:
-        if lidstatus.isclosed():
-            exit(0)
+        for i in range(20):
+            lidstatus=p.getlidstatus()
+            logging.info( "Lid status: %s"%str(lidstatus))
+            if cmd=="OPEN":
+                if lidstatus.isopen():
+                    exit(0)
+            else:
+                if lidstatus.isclosed():
+                    exit(0)
 
-    time.sleep(2)
+            time.sleep(2)
+    except ValueError as exc:
+        logging.warning("LID operation failed %d times: Exception: %s"%(attempt+1,str(exc)))
+        # Flush buffer
+        while p.readline()!="":
+            print("Flushed line")
+        logging.info("Status before clear: %s"%str(p.getstatus()))
+        p.clearerrors()
+        logging.info("Status after clear: %s"%str(p.getstatus()))
+        if attempt>0:
+            # Reopen
+            logging.info("Closing connection")
+            p.close()
+            logging.info("Reopening connection")
+            p.open()
+            logging.info("Lid Temp=%f, Block temp=%f"%(p.getlidtemp(), p.gettemp()))
+
+        logging.info("Sleeping %d seconds..."%(10*attempt))
+        time.sleep(10*attempt)
+        logging.info("Retrying")
 
 logging.error( "Lid is not completing operation")
 exit(1)
