d2c5901b92a556bee29c5a8a1e231cf155e0fa31
Brent Townshend <bst@tc.com>
2018-11-05 23:02:01 -0800

    Remove kludge that aliased magplate location with sample plate
    But may break use of magplate

--- parselog/datalog.py
+++ parselog/datalog.py
@@ -80,9 +80,9 @@ class Datalog(object):
         sample.SHOWTIPS = True
         
     def logop(self,op,tip,vol,wellx,welly,rack,grid,pos,lc,std,volset,isMulti):
-        if grid==18 and pos==2:   # Make magplate refer to samples
-            grid=4
-            pos=3
+        # if grid==18 and pos==2:   # Make magplate refer to samples
+        #     grid=4
+        #     pos=3
             
         entry=LogEntry(op,tip,vol,wellx,welly,rack,grid,pos,lc,std,volset,isMulti)
         sname=entry.sample.name
