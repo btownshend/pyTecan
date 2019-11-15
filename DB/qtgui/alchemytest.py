51803db5e3e15d61320e5e0b3808eb4dfd2e88b7
Brent Townshend <bst@tc.com>
2019-11-15 12:37:48 -0800

    Update alchemytest.py

--- DB/qtgui/alchemytest.py
+++ DB/qtgui/alchemytest.py
@@ -18,6 +18,10 @@ class Programs(Base):
     gitlabel = Column(String)
     totaltime = Column(Float)
     complete = Column(Boolean)
+    runs = relationship("Runs", back_populates="program")
+    ops = relationship("Ops", back_populates="program")
+    samples = relationship("Samples", back_populates="program")
+
     def __repr__(self):
         return "<Program(program=%d, name='%s')>" % (self.program, self.name)
 
@@ -25,7 +29,7 @@ class Runs(Base):
     __tablename__ = 'runs'
     run = Column(Integer, primary_key=True)
     program_id = Column(Integer, ForeignKey('programs.program'), name="program")
-    program = relationship("Programs",back_populates='runs')
+    program = relationship("Programs",back_populates='runs', uselist=False, lazy="selectin")
     logfile = Column(String)
     starttime = Column(Date)
     endtime = Column(Date)
@@ -34,21 +38,80 @@ class Runs(Base):
     expt = Column(Integer)
     logheader = Column(String)
     status = Column(String)
+    vols = relationship("Vols", back_populates="run")
 
     def __repr__(self):
         return "<Run(run='%s', program='%s', logfile='%s', start='%s')>" % \
                (self.run, self.program, self.logfile, self.starttime)
 
+class Samples(Base):
+    __tablename__="samples"
+    sample = Column(Integer, primary_key=True)
+    program_id = Column(Integer, ForeignKey('programs.program'), name="program")
+    program = relationship("Programs",back_populates='samples', uselist=False)
+    plate = Column(String)
+    well = Column(String)
+    name = Column(String)
+    def __repr__(self):
+        return "<Sample(%s@%s.%s)>" % (self.name, self.plate, self.well)
 
-Programs.runs = relationship("Runs", order_by=Runs.run, back_populates="program")
+class LiquidClasses(Base):
+    __tablename__='liquidclasses'
+    lc = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    def __repr__(self):
+        return "<LC(%s)>"%(self.name)
+
+class Ops(Base):
+    __tablename__='ops'
+    op=Column(Integer,primary_key=True)
+    program_id=Column(Integer, ForeignKey('programs.program'), name="program")
+    program=relationship("Programs",back_populates='ops')
+    sample_id=Column(Integer, ForeignKey('samples.sample'), name="sample")
+    cmd=Column(String)
+    lineno=Column(Integer)
+    elapsed=Column(Float)
+    tip=Column(Integer)
+    lc_id=Column(Integer, ForeignKey('liquidclasses.lc'), name='lc')
+    lc=relationship("LiquidClasses", uselist=False, lazy="joined")
+    volchange=Column(Float)
+    clean=Column(Boolean)
+    def __repr__(self):
+        return "<Op(%s@%d)>"%(self.cmd,self.lineno)
+
+class Vols(Base):
+    __tablename__='vols'
+
+    vol=Column(Integer, primary_key=True)
+    run_id=Column(Integer, ForeignKey('runs.run'), name="run")
+    run=relationship("Runs",back_populates='vols', uselist=False)
+    op_id=Column(Integer, ForeignKey('ops.op'), name="op")
+    op=relationship("Ops",uselist=False,lazy="joined")
+    estvol=Column(Float)
+    gemvolume=Column(Float)
+    volume=Column(Float)
+    height=Column(Integer)
+    submerge=Column(Integer)
+    zmax=Column(Integer)
+    zadd=Column(Integer)
+    measured=Column(Date)
+    def __repr__(self):
+        return "<Vol(%d,%d,%s)>"%(self.run_id,self.op_id, self.volume)
 
 engine = create_engine("mysql+pymysql://ngsreadonly:@35.203.151.202/robot", echo=True)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-for run  in session.query(Runs).options(selectinload(Runs.program)):
+runs=session.query(Runs)
+for run in runs[0:10]:
     print(run, run.program)
+    if run.program is not None:
+        for op in run.program.ops[0:3]:
+            print(" ",op,op.lc)
+    for vol in run.vols[10:13]:
+        print(" ",vol.op,vol.op.lc,vol)
 
 
-# for program  in session.query(Programs):
-#     print(program)
+for program  in session.query(Programs):
+     print(program)
