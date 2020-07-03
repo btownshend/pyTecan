23aa0c850a005371f1c81b0dedc2ebe30aaa4975
Brent Townshend <bst@tc.com>
2020-07-02 20:12:08 -0700

    Added carrier.py, gemfile.py

---
+++ Misc/carrier.py
@@ -0,0 +1,173 @@
+# Handle Tecan Gemini Carriers and Racks
+# Loads data from Carriers.cfg
+import pprint
+
+import click
+
+from Experiment.plate import Plate
+from Experiment import decklayout
+
+class Carrier(object):
+    def __init__(self):
+        self.checksum=None
+        self.timestamp=None
+        self.username=None
+        self.carriers=[]
+        self.racks=[]
+        self.vectors=[]
+        self.data999=None
+
+    def findrack(self,name):
+        """Locate a rack by name"""
+        for r in self.racks:
+            if r["name"]==name:
+                return r
+        print(f"Rack with name {name} not found.")
+        return None
+
+    def findcarrier(self,name):
+        """Locate a carrier by name"""
+        for c in self.carriers:
+            if c["name"]==name:
+                return c
+        print(f"Carrier with name {name} not found.")
+        return None
+
+    def getcarrier(self,id):
+        """Locate a carrier by id"""
+        for c in self.carriers:
+            if c["id"]==id:
+                return c
+        print(f"Carrier with ID {id} not found.")
+        return None
+
+    def print(self):
+        print(f"Checksum: {self.checksum}, Timestamp: {self.timestamp}, User: {self.username}")
+        print(f"Data999: {self.data999}")
+        print("Carriers:")
+        for c in self.carriers:
+            pprint.pprint(c)
+
+        print("\nRacks:")
+        for r in self.racks:
+            pprint.pprint(r)
+
+        print("\nVectors:")
+        for c in self.vectors:
+            pprint.pprint(c)
+
+
+    def loadCFG(self, filename):
+        with open(filename,"r") as fd:
+            self.checksum = fd.readline().strip()
+            self.timestamp = fd.readline().strip()
+            fd.readline()  # Blank?
+            self.username = fd.readline().strip()
+            lineno=4
+            for line in fd:
+                lineno+=1
+                line=line.strip()
+                #print(f"line: {line}")
+                fields=line.split(";")
+                if fields[-1]=="":
+                    fields=fields[:-1]
+                #print(f"{fields[0]}: {fields[1]} {len(fields)-2}")
+                if fields[0]=="999":
+                    cont=[]
+                    self.data999=fields[1:]
+                elif fields[0]=="13":  # Carriers
+                    cont=[]
+                    carrier = {"name":fields[1],
+                               "id":int(fields[2].split("/")[0]),
+                               "barcode": int(fields[2].split("/")[1]),
+                               "refoffset": [float(f)/10 for f in fields[3].split("/")],
+                               "dimensions": [float(f)/10 for f in fields[4].split("/")],
+                               "nsites": int(fields[5]),
+                               "other":fields[7:],
+                               "cont":cont}
+                    carrier["romaonly"]=(carrier["barcode"]==0)
+                    typecode=int(fields[6])
+                    if typecode==0:
+                        carrier["type"]="Standard"
+                    elif typecode==2:
+                        carrier["type"]="Hidden"
+                    elif typecode==-3:
+                        carrier["type"]="ROMA"
+                    elif typecode == 13:
+                        carrier["type"] = "Carousel"
+                    else:
+                        print(f"Unknown carrier type code: {typecode} at line {lineno}")
+                        carrier["type"]=typecode
+                    self.carriers.append(carrier)
+                elif fields[0]=="15":  # Racks
+                    cont=[]
+                    assert(int(fields[2])==0)  # Unknown what it is, but always zero
+                    rack = {"name":fields[1],
+                            #"unk1":int(fields[2]),
+                            "wells":[int(f) for f in fields[3].split("/")],
+                            "wellpos":[int(f) for f in fields[4].split("/")],
+                            "zcoords":{k: (2100-int(f))/10.0 for k, f in zip(['max','start','dispense','travel'], fields[5].split("/"))},
+                            "area":float(fields[6]),
+                            "tipsperwell":int(fields[7]),
+                            "tiptouchdist":float(fields[8]),
+                            "piercing":[int(f) for f in fields[9].split("/")],
+                            "type":int(fields[10]),
+                            "diti":{"capacity":float(fields[11]),"offset":float(fields[12])}, # TODO: int?
+                            "depth":float(fields[13]),
+                            "precise":{"active":int(fields[14]),"xstart":int(fields[15]),"ystart":int(fields[16]),"speed":float(fields[17])},
+                            "npos": int(fields[18]),
+                            "other":fields[19:],
+                            "cont":cont}
+                    self.racks.append(rack)
+                elif fields[0]=="17":  # ROMA Vector
+                    cont=[]
+                    vector = {"name":fields[1],
+                              "grip": {"gripdist":float(fields[2].split("/")[0])/10,
+                                       "reldist": float(fields[2].split("/")[1]) / 10,
+                                       "force": float(fields[2].split("/")[2]),
+                                       "speed": float(fields[2].split("/")[3]) / 10},
+                              "xyzspeed": float(fields[3].split("/")[0])/10,
+                              "rotspeed": float(fields[3].split("/")[1])/10,
+                              "nsteps": int(fields[4]),
+                              "carrierid": int(fields[5]),
+                              "other":fields[6:],
+                              "cont":cont}
+                    self.vectors.append(vector)
+                elif fields[0]=="20":
+                    #print(f"{fields[0]}: {fields[1]} {len(fields)-2}")
+                    cont=[]
+                elif fields[0]=="998":
+                    cont.append([int(fn) for f in fields[1:] for fn in f.split("/") ])
+                else:
+                    print(f"Unknown field code {fields[0]} at line {lineno}")
+        # Clean up continutation lines
+        for v in self.vectors:
+            assert(v["nsteps"] == len(v["cont"]))
+            v["steps"]=[{"x":d[0]/10.0,"y":d[1],"z":d[2]/10.0,"r":(d[3]%10000)/10.0,"abs":d[3]>=10000} for d in v["cont"]]
+            del v["cont"]
+            del v["nsteps"] # Redundant
+        for c in self.carriers:
+            assert(c["nsites"]+1 == len(c["cont"]))
+            c["sites"]=[{"shape":d[0],"xsize":d[1]/10.0,"ysize":d[2]/10.0,"xoffset":d[3]/10.0,"yoffset":d[4]/10.0,"zoffset":d[5]/10.0} for d in c["cont"][:-1]]
+            c["cont"]=c["cont"][-1]   # Unsure what this last line is for
+            del c["nsites"]
+        for r in self.racks:
+            assert(r["npos"] == len(r["cont"]))
+            r["allowed"]=[{"carrier":d[0],"positions":[p+1 for p in range(10) if d[1]&(1<<p)]} for d in r["cont"]]
+            if r["npos"]>0:
+                r["unk"]=r["cont"][-1][2]
+            del r["npos"]
+            del r["cont"]
+
+
+@click.command()
+@click.option('--filename','-f',help="File to load")
+@click.option('--dump','-d',is_flag=True,help="Dump contents")
+def main(filename,dump):
+    carrier = Carrier()
+    carrier.loadCFG(filename)
+    if dump:
+        carrier.print()
+
+if __name__ == '__main__':
+    main()
---
+++ Misc/gemfile.py
@@ -0,0 +1,153 @@
+# Handle Tecan Gemini gem file
+# Loads data from beginning of gem file (CFG part)
+
+import click
+
+from Experiment.plate import Plate
+from Experiment import decklayout
+import pprint
+from Misc.carrier import Carrier
+
+
+class GemFile(object):
+    def __init__(self):
+        self.checksum=None
+        self.timestamp=None
+        self.username=None
+        self.layout=[]
+
+    def print(self, verbose=False):
+        print(f"Checksum: {self.checksum}, Timestamp: {self.timestamp}, User: {self.username}")
+        print(f"Data999: {self.data999}")
+        print(f"Resline: {self.resline}")
+        for r in self.layout:
+            print("---------")
+            print(f"grid {r['grid']}: carrier:{r['carrier']['name']}")
+            for p in r['racks']:
+                print(p)
+                print(f"\t{p['pos']}\t{p['name']}\t{p['rack']['name']}")
+            if verbose:
+                pprint.pprint(r)
+
+
+    def save(self, filename):
+        """Save as .gem file up to part where we put in actual program commands """
+        with open(filename,"w") as fd:
+            fd.write(f"{self.checksum}\r\n")
+            fd.write(f"{'%-32.32s'%self.timestamp}\r\n")
+            fd.write(f"{'%-128.128s'%''}\r\n")
+            fd.write(f"{'%-128.128s'%self.username}\r\n")
+            fd.write("--{ RES }--\r\n")
+            fd.write(f"{self.resline}\r\n")
+            fd.write("--{ CFG }--\r\n")
+            fd.write(f"999;{';'.join(['%s'%d for d in self.data999])};\r\n")
+            carrierList=[-1 if c is None else c["id"] for c in self.carriers]
+            fd.write(f"14;{';'.join(['%s'%c for c in carrierList])};\r\n")
+            for grid in range(len(carrierList)):
+                racks=None
+                for l in self.layout:
+                    if l["grid"]==grid:
+                        racks=l["racks"]
+                if racks is not None:
+                    fd.write(f"998;{len(racks)};{';'.join([r['rack']['name'] for r in racks])};\r\n")
+                    fd.write(f"998;{';'.join([r['name'] for r in racks])};\r\n")
+                else:
+                    fd.write("998;0;\r\n")
+            fd.write("--{ RPG }--\r\n")
+
+
+    def load(self, filename, carrier):
+        with open(filename,"r") as fd:
+            self.checksum = fd.readline().strip()
+            self.timestamp = fd.readline().strip()
+            assert(fd.readline().strip()=="")  # Blank?
+            self.username = fd.readline().strip()
+            assert(fd.readline().strip()=="--{ RES }--")
+            self.resline = fd.readline().strip()
+            assert(fd.readline().strip()=="--{ CFG }--")
+
+            lineno=7
+            gridpos=0
+            for line in fd:
+                lineno+=1
+                line=line.strip()
+                if line=='--{ RPG }--':
+                    break
+                #print(f"line: {line}")
+                fields=line.split(";")
+                if fields[-1]=="":
+                    fields=fields[:-1]
+                #print(f"{fields[0]}: {fields[1]} {len(fields)-2}")
+                if fields[0]=="999":
+                    self.data999=fields[1:]
+                elif fields[0]=="14":  # Carrier list
+                    self.carriers=[carrier.getcarrier(int(f)) if int(f)!=-1 else None for f in fields[1:] ]
+                elif fields[0]=="998":  # Contents of each grid pos
+                    if int(fields[1])>0:
+                        racks=[carrier.findrack(f) for f in fields[2:]]
+                        assert(len(racks)==int(fields[1]))
+                        line2=fd.readline().strip()
+                        names=line2.split(';')[1:]
+                        assert(all([n=='' for n in names[len(racks):]]))  # Sometime get blank names at end
+                        names=names[:len(racks)]
+                        self.layout.append({'grid':gridpos, 'racks':[{'pos':(i+1),'rack':r,'name':names[i]} for i,r in enumerate(racks)],'carrier':self.carriers[gridpos]})
+                    gridpos += 1
+                else:
+                    print(f"Unknown field code {fields[0]} at line {lineno}")
+
+    def deckcompare(self, carrier: Carrier):
+        for plate in Plate.allPlates():
+            print('\n-------------')
+            print(f"Plate: {plate}, Type: {plate.plateType}, Loc: {plate.location.carrierName}")
+            r = carrier.findrack(plate.plateType.name)
+            c = carrier.findcarrier(plate.location.carrierName)
+            print(f"\tCarrier: {c}")
+            print(f"\tRack:    {r}")
+            print(f"zoffset (from deck):")
+            zoffset1=(2100 - plate.getzmax()) / 10.0
+            print(f"\tdecklayout.py:{plate.getzmax()} -> {zoffset1}")
+            coffsets = [c['refoffset'][2], c['sites'][plate.location.pos - 1]['zoffset']]
+            print(f"\tcarrier:ref:{coffsets[0]},pos[{plate.location.pos}]:{coffsets[1]}", end='')
+            roffset = r['zcoords']['max']
+            print(f" + rack:{roffset}", end='')
+            tipoffset = 39
+            print(f" + tip:{tipoffset}", end='')
+            print(f" -> {coffsets[0] + coffsets[1] + roffset + tipoffset}")
+            if coffsets[0]+coffsets[1]+roffset+tipoffset != zoffset1:
+                print("**** Z-Offset mismatch!")
+            print(f"Location:")
+            print(f"\tdecklayout.py: {plate.location.grid},{plate.location.pos}")
+            for l in self.layout:
+                if l["grid"]==plate.location.grid:
+                    for lr in l["racks"]:
+                        if lr["pos"]==plate.location.pos:
+                            print(f"Found gemfile entry: carrier: {l['carrier']['name']}, rack: {lr['rack']['name']}")
+                            if l['carrier']!=c:
+                                print("**** Carrier mismatch!")
+                            if lr['rack'] != r:
+                                print("**** Rack mismatch!")
+
+
+
+
+@click.command()
+@click.option('--filename','-f',help="File to load")
+@click.option('--output','-o',help="File to write")
+@click.option('--cfilename','-c',help="Carrier.cfg filename")
+@click.option('--dump','-d',is_flag=True,help="Dump contents")
+@click.option('--verbose', '-v', is_flag=True, help="Dump contents")
+@click.option('--compare', '-C', is_flag=True, help="Compare to decklayout")
+def main(filename,cfilename,compare,dump,verbose,output):
+    carrier = Carrier()
+    carrier.loadCFG(cfilename)
+    gem = GemFile()
+    gem.load(filename,carrier)
+    if dump:
+        gem.print(verbose)
+    if compare:
+        gem.deckcompare(carrier)
+    if output != None:
+        gem.save(output)
+
+if __name__ == '__main__':
+    main()
