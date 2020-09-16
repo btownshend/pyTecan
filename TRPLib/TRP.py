62d338835c40930e08e41b15181aae41173bd3ed
Brent Townshend <bst@tc.com>
2020-09-15 22:58:10 -0700

    Restore userprompt at end of run

--- TRPLib/TRP.py
+++ TRPLib/TRP.py
@@ -199,7 +199,7 @@ class TRP(object):
     def finish(self):
         self.e.lihahome()
         db.endrun()   # May have already been ended before waiting to turn off reagent chiller; idempotent
-        #worklist.userprompt("Process complete. Continue to turn off reagent cooler")
+        worklist.userprompt("Process complete.",1)
         Sample.evapcheckallsamples()   #  Run an evap check to ensure that we compute with correct reagent block temp
         self.e.setreagenttemp(None)
 
